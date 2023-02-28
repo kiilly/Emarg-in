19956a0efce0c125ad5e39afd8481e6934595bab
Koven <koven@MacBook-Pro-de-Koven.local>
2023-02-28 15:22:16 +0100

    QRcode list

--- my_app/lib/second.dart
+++ my_app/lib/second.dart
@@ -1,32 +1,122 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class Second extends StatefulWidget {
-  
-
   @override
   State<Second> createState() => _SecondState();
 }
 
 class _SecondState extends State<Second> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+  List<String> scannedQRList = [];
+
+  Future<void> scanQR() async {
+    String? qrResult = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => QRViewPage()),
+    );
+    if (qrResult != null) {
+      setState(() {
+        scannedQRList.add(qrResult);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Scanner'),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Welcome"),
-              SizedBox(height: 50,),
-              OutlinedButton.icon(onPressed: () {},
-              icon: Icon(
-                Icons.exit_to_app, 
-                size: 18,
-              ), 
-              label: Text('Logout'),)
+              SizedBox(
+                height: 50,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  scanQR();
+                },
+                child: Text('Scan QR Code'),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('Scanned QR Codes:'),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: scannedQRList.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(scannedQRList[index]),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
-          )),
+        ),
+      ),
     );
+  }
+}
+
+class QRViewPage extends StatefulWidget {
+  const QRViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<QRViewPage> createState() => _QRViewPageState();
+}
+
+class _QRViewPageState extends State<QRViewPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan de code QR'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      if (Navigator.of(context).canPop()) {
+        return;
+      }
+
+      setState(() {
+        result = scanData;
+      });
+
+      Navigator.pop(context, result!.code);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
