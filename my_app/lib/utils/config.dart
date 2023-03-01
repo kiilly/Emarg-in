7f5fab9af70b9f90f03fe3a8ba02dbc9e2adea66
Chemllal-Anas <114062242+Chemllal-Anas@users.noreply.github.com>
2023-03-01 13:40:30 +0100

    test

---
+++ my_app/lib/utils/config.dart
@@ -0,0 +1,46 @@
+//set constant configuration
+
+import 'package:flutter/material.dart'
+
+class config{ 
+    static MediaQueryData? MediaQueryData;
+    static double? screenWidth;
+    static double? screenHeight;
+
+    void init(BuildContext context){
+        MediaQueryData = MediaQueryData.of(context);
+        screenWidth = MediaQueryData!.size.width;
+        screenHeight = MediaQueryData!.size.height;
+
+    }
+
+
+static get widthSize{
+    return screenWidth;
+}
+
+static get heightSize{
+    return screenHeight;
+}
+
+//defining space height
+
+static const spaceSmall = sizedBox(height: 25,);
+static final spaceMedium = sizedBox(height: screenHeight! * 0.05,);
+static final spaceBig = sizedBox(height: screenHeight! * 0.08,);
+
+//text field border
+static const outlinedBorder = OutlinedBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+);
+static const focusBorder = OutlinedBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(
+        color: Colors.greenAccent,
+    ));
+static const errorBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(8)),
+    borderSide: BorderSide(
+        color: Colors.red,
+    ));  //BorderSide   //OutlineInputBorder
+}
---
+++ my_app/lib/widgets/logobar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class MyNavigationBar extends StatefulWidget {
+  const MyNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  _MyNavigationBarState createState() => _MyNavigationBarState();
+}
+
+class _MyNavigationBarState extends State<MyNavigationBar> {
+  // Add your navigation bar code here
+}
+
