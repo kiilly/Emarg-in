30af0a2ee2efc3ee50ae5ac49d569dce7a956179
Koven <koven@MacBook-Pro-de-Koven.local>
2023-03-01 10:33:18 +0100

    QR list

---
+++ my_app/lib/ScannedCodesListPage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ScannedCodesListPage extends StatelessWidget {
+  final List<String> scannedCodes;
+
+  ScannedCodesListPage({required this.scannedCodes});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Liste des codes scannés'),
+      ),
+      body: ListView.builder(
+        itemCount: scannedCodes.length,
+        itemBuilder: (context, index) {
+          String code = scannedCodes[index];
+          String tag = (code.endsWith("812")) ? ": APE" : (code.endsWith("813")) ? ": APAE" : "";
+          return ListTile(
+            title: Text(code + tag),
+          );
+        },
+      ),
+    );
+  }
+}
--- my_app/lib/second.dart
+++ my_app/lib/second.dart
@@ -1,82 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:my_app/ScannedCodesListPage.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class Second extends StatefulWidget {
   @override
-  State<Second> createState() => _SecondState();
+  _SecondState createState() => _SecondState();
 }
 
 class _SecondState extends State<Second> {
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
-  QRViewController? controller;
-  List<String> scannedQRList = [];
-
-  Future<void> scanQR() async {
-    String? qrResult = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => QRViewPage()),
-    );
-    if (qrResult != null) {
-      setState(() {
-        scannedQRList.add(qrResult);
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('QR Code Scanner'),
-      ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  scanQR();
-                },
-                child: Text('Scan QR Code'),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text('Scanned QR Codes:'),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: scannedQRList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(scannedQRList[index]),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class QRViewPage extends StatefulWidget {
-  const QRViewPage({Key? key}) : super(key: key);
-
-  @override
-  State<QRViewPage> createState() => _QRViewPageState();
-}
-
-class _QRViewPageState extends State<QRViewPage> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  Barcode? result;
+  List<String> scannedCodes = [];
   QRViewController? controller;
 
   @override
@@ -85,16 +19,50 @@ class _QRViewPageState extends State<QRViewPage> {
       appBar: AppBar(
         title: const Text('Scan de code QR'),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-            ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 5,
+                child: QRView(
+                  key: qrKey,
+                  onQRViewCreated: _onQRViewCreated,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: (scannedCodes.isNotEmpty)
+                      ? Text(
+                          'Données : ${scannedCodes.last}')
+                      : const Text('Scannez un code'),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              OutlinedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ScannedCodesListPage(
+                        scannedCodes: scannedCodes,
+                      ),
+                    ),
+                  );
+                },
+                icon: Icon(
+                  Icons.list,
+                  size: 18,
+                ),
+                label: Text('Liste des codes'),
+              )
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
@@ -102,15 +70,11 @@ class _QRViewPageState extends State<QRViewPage> {
   void _onQRViewCreated(QRViewController controller) {
     this.controller = controller;
     controller.scannedDataStream.listen((scanData) {
-      if (Navigator.of(context).canPop()) {
-        return;
+      if (!scannedCodes.contains(scanData.code)) {
+        setState(() {
+          scannedCodes.add(scanData.code!);
+        });
       }
-
-      setState(() {
-        result = scanData;
-      });
-
-      Navigator.pop(context, result!.code);
     });
   }
 
